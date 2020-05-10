add08132deed50838e978af4e03c872701f22b28
Abdulrahman Mazhar <abmazhr@gmail.com>
2020-05-11 01:17:30 +0200

    Add DatabaseInterface module for any database implementations

---
+++ users/src/application/infrastructure/persistence/gateway/database.py
@@ -0,0 +1,81 @@
+from abc import ABCMeta, abstractmethod
+
+from src.application.entity.database_user import DatabaseUser
+from src.application.types import (
+    Maybe,
+    Either,
+    SimpleConfig
+)
+from src.domain.entity.failure import Failure
+from src.domain.entity.success import Success
+from src.domain.entity.user import User as DomainUser
+
+
+class DatabaseInterface(metaclass=ABCMeta):
+    # maybe having a better way of doing this later ;)
+    class FetchBy:
+        @abstractmethod
+        def id(self, *, user_id: str) -> Either[Failure, DatabaseUser]: pass
+
+        @abstractmethod
+        def name(self, *, user_name: str) -> Either[Failure, DatabaseUser]: pass
+
+        @abstractmethod
+        def email(self, *, user_email: str) -> Either[Failure, DatabaseUser]: pass
+
+    class UpdateBy:
+        @abstractmethod
+        def id(self, *, user_id: str, updated_user: DomainUser) -> Either[Failure, Success]: pass
+
+        @abstractmethod
+        def name(self, *, user_name: str, updated_user: DomainUser) -> Either[Failure, Success]: pass
+
+        @abstractmethod
+        def email(self, *, user_email: str, updated_user: DomainUser) -> Either[Failure, Success]: pass
+
+    class DeleteBy:
+        @abstractmethod
+        def id(self, *, user_id: str) -> Either[Failure, Success]: pass
+
+        @abstractmethod
+        def name(self, *, user_name: str) -> Either[Failure, Success]: pass
+
+        @abstractmethod
+        def email(self, *, user_email: str) -> Either[Failure, Success]: pass
+
+    @abstractmethod
+    def __init__(self, *, config: Maybe[SimpleConfig]) -> None:
+        self.fetch_user_by = self._fetch_user_by()
+        self.update_user_by = self._update_user_by()
+        self.delete_user_by = self._delete_user_by()
+
+    @staticmethod
+    def from_domain_user_to_database_user(*, user: DomainUser, user_id: str) -> DatabaseUser:
+        return DatabaseUser(
+            id=user_id,
+            name=user.name,
+            age=user.age,
+            email=user.email,
+            password=user.password
+        )
+
+    @staticmethod
+    def from_database_user_to_domain_user(*, user: DatabaseUser) -> DomainUser:
+        return DomainUser(
+            name=user.name,
+            age=user.age,
+            email=user.email,
+            password=user.password
+        )
+
+    @abstractmethod
+    def persist_user(self, *, user: DomainUser) -> Either[Failure, DatabaseUser]: pass
+
+    @abstractmethod
+    def _fetch_user_by(self) -> 'DatabaseInterface.FetchBy': pass
+
+    @abstractmethod
+    def _update_user_by(self) -> 'DatabaseInterface.UpdateBy': pass
+
+    @abstractmethod
+    def _delete_user_by(self) -> 'DatabaseInterface.DeleteBy': pass
