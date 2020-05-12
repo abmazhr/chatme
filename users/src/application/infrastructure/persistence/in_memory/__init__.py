b3c58802ccbe16d035e99de91d1f74a15befdb94
Abdulrahman Mazhar <abmazhr@gmail.com>
2020-05-12 22:49:03 +0200

    Update InMemory implementation to reflect the new PersistenceInterface

--- users/src/application/infrastructure/persistence/in_memory/__init__.py
+++ users/src/application/infrastructure/persistence/in_memory/__init__.py
@@ -1,5 +1,5 @@
-from src.application.entity.database_user import DatabaseUser
-from src.application.infrastructure.persistence.gateway.database import DatabaseInterface
+from src.application.entity.user import ApplicationUser
+from src.application.infrastructure.persistence import PersistenceInterface
 from src.application.types import (
     Maybe,
     Either,
@@ -8,15 +8,14 @@ from src.application.types import (
 from src.application.utilities.functions import exception_handler
 from src.domain.entity.failure import Failure
 from src.domain.entity.success import Success
-from src.domain.entity.user import User as DomainUser
+from src.domain.entity.user import DomainUser as DomainUser
 
 
-class InMemoryDatabase(DatabaseInterface):
-    # maybe later will modularize the functions in modules to be easier to maintain ;)
+class InMemoryDatabase(PersistenceInterface):
     def __init__(self, *, config: Maybe[SimpleConfig]) -> None:
-        super().__init__(config=config)
         self.__db = dict(ids={}, names={}, emails={})
         self.__last_id = 0  # just simple increment but in real db it's more complicated xD
+        super().__init__(config=config)
         """
         For example the DB will look like these references
         {
@@ -37,58 +36,62 @@ class InMemoryDatabase(DatabaseInterface):
         }
         """
 
+    # maybe later will modularize the functions in modules to be easier to maintain ;)
+
     @exception_handler
-    def persist_user(self, *, user: DomainUser) -> Either[Failure, DatabaseUser]:
-        persisted_user = DatabaseInterface.from_domain_user_to_database_user(
+    def persist_user(self, *, user: DomainUser) -> Either[Failure, ApplicationUser]:
+        persisted_user = PersistenceInterface.from_domain_user_to_database_user(
             user=user,
             user_id=str(self.__last_id)
         )
         self.__db['ids'][str(self.__last_id)] = persisted_user
-        self.__db['names'][user.name] = self.__last_id
-        self.__db['emails'][user.email] = self.__last_id
+        self.__db['names'][user.name] = str(self.__last_id)
+        self.__db['emails'][user.email] = str(self.__last_id)
         self.__last_id += 1
 
         return persisted_user
 
-    def _fetch_user_by(self) -> 'DatabaseInterface.FetchBy':
+    def _fetch_user_by(self) -> 'PersistenceInterface.FetchBy':
         db = self.__db
 
-        class InMemoryFetchBy(DatabaseInterface.FetchBy):
+        class InMemoryFetchBy(PersistenceInterface.FetchBy):
             @exception_handler
-            def id(self, *, user_id: str) -> Either[Failure, DatabaseUser]:
-                fetch_status: Maybe[DatabaseUser] = db["ids"].get(user_id)
-                if isinstance(fetch_status, DatabaseUser):
+            def id(self, *, user_id: str) -> Either[Failure, ApplicationUser]:
+                fetch_status: Maybe[ApplicationUser] = db["ids"].get(user_id)
+                if isinstance(fetch_status, ApplicationUser):
                     return fetch_status
 
                 return Failure(error=f"There is no user with id {user_id} to be fetched")
 
             @exception_handler
-            def name(self, *, user_name: str) -> Either[Failure, DatabaseUser]:
-                fetch_status: Maybe[DatabaseUser] = db["names"].get(user_name)
-                if isinstance(fetch_status, DatabaseUser):
-                    return fetch_status
+            def name(self, *, user_name: str) -> Either[Failure, ApplicationUser]:
+                user_id: str = db["names"].get(user_name)
+                if user_id is not None:
+                    user: ApplicationUser = db["ids"][user_id]
+                    return user
 
                 return Failure(error=f"There is no user with name {user_name} to be fetched")
 
             @exception_handler
-            def email(self, *, user_email: str) -> Either[Failure, DatabaseUser]:
-                fetch_status: Maybe[DatabaseUser] = db["emails"].get(user_email)
-                if isinstance(fetch_status, DatabaseUser):
-                    return fetch_status
+            def email(self, *, user_email: str) -> Either[Failure, ApplicationUser]:
+                user_id: str = db["emails"].get(user_email)
+                if user_id is not None:
+                    user: ApplicationUser = db["ids"][user_id]
+                    return user
 
                 return Failure(error=f"There is no user with email {user_email} to be fetched")
 
         return InMemoryFetchBy()
 
-    def _update_user_by(self) -> 'DatabaseInterface.UpdateBy':
+    def _update_user_by(self) -> 'PersistenceInterface.UpdateBy':
         db = self.__db
 
-        class InMemoryUpdateBy(DatabaseInterface.UpdateBy):
+        class InMemoryUpdateBy(PersistenceInterface.UpdateBy):
             @exception_handler
             def id(self, *, user_id: str, updated_user: DomainUser) -> Either[Failure, Success]:
-                fetch_status: Maybe[DatabaseUser] = db["ids"].get(user_id)
-                if isinstance(fetch_status, DatabaseUser):
-                    db["ids"][fetch_status.id] = DatabaseInterface.from_domain_user_to_database_user(
+                fetch_status: Maybe[ApplicationUser] = db["ids"].get(user_id)
+                if isinstance(fetch_status, ApplicationUser):
+                    db["ids"][fetch_status.id] = PersistenceInterface.from_domain_user_to_database_user(
                         user=updated_user,
                         user_id=fetch_status.id
                     )
@@ -100,7 +103,7 @@ class InMemoryDatabase(DatabaseInterface):
             def name(self, *, user_name: str, updated_user: DomainUser) -> Either[Failure, Success]:
                 user_id: Maybe[str] = db["names"].get(user_name)
                 if user_id is not None:
-                    db["ids"][user_id] = DatabaseInterface.from_domain_user_to_database_user(
+                    db["ids"][user_id] = PersistenceInterface.from_domain_user_to_database_user(
                         user=updated_user,
                         user_id=user_id
                     )
@@ -112,7 +115,7 @@ class InMemoryDatabase(DatabaseInterface):
             def email(self, *, user_email: str, updated_user: DomainUser) -> Either[Failure, Success]:
                 user_id: Maybe[str] = db["emails"].get(user_email)
                 if user_id is not None:
-                    db["ids"][user_id] = DatabaseInterface.from_domain_user_to_database_user(
+                    db["ids"][user_id] = PersistenceInterface.from_domain_user_to_database_user(
                         user=updated_user,
                         user_id=user_id
                     )
@@ -123,12 +126,12 @@ class InMemoryDatabase(DatabaseInterface):
 
         return InMemoryUpdateBy()
 
-    def _delete_user_by(self) -> 'DatabaseInterface.DeleteBy':
+    def _delete_user_by(self) -> 'PersistenceInterface.DeleteBy':
         db = self.__db
 
-        class InMemoryDeleteBy(DatabaseInterface.DeleteBy):
+        class InMemoryDeleteBy(PersistenceInterface.DeleteBy):
             @exception_handler
-            def __inner_delete_user(self, *, user: DatabaseUser) -> Either[Failure, Success]:
+            def __inner_delete_user(self, *, user: ApplicationUser) -> Either[Failure, Success]:
                 del db["ids"][user.id]
                 del db["names"][user.name]
                 del db["emails"][user.email]
@@ -137,8 +140,8 @@ class InMemoryDatabase(DatabaseInterface):
 
             @exception_handler
             def id(self, *, user_id: str) -> Either[Failure, Success]:
-                fetch_status: Maybe[DatabaseUser] = db["ids"].get(user_id)
-                if isinstance(fetch_status, DatabaseUser):
+                fetch_status: Maybe[ApplicationUser] = db["ids"].get(user_id)
+                if isinstance(fetch_status, ApplicationUser):
                     self.__inner_delete_user(user=fetch_status)
                     return Success()
 
@@ -163,3 +166,7 @@ class InMemoryDatabase(DatabaseInterface):
                 return Failure(error=f"There is no user with email {user_email} to be deleted")
 
         return InMemoryDeleteBy()
+
+
+db = InMemoryDatabase(config=None)
+print(db)
