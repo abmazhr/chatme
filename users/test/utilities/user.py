45a947d86f3cee83c330c3a6ee31c1152c05a230
Abdulrahman Mazhar <abmazhr@gmail.com>
2020-05-20 01:45:21 +0200

    Update user utilities for tests

--- users/test/utilities/user.py
+++ users/test/utilities/user.py
@@ -8,12 +8,3 @@ def generate_valid_domain_user():
         password="Str0ngPassword",
         email="test@test.com"
     )
-
-
-def generate_invalid_domain_user():
-    return create_user(
-        name="test",
-        age=26,
-        password="weak_password",
-        email="test@test.com"
-    )
