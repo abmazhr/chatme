27641ff9c44c44953886acf6e7f4ec381026e409
Abdulrahman Mazhar <abmazhr@gmail.com>
2020-05-20 01:38:54 +0200

    Add test health check

---
+++ users/test/integration/application/web/rest_api/starlette/test_health_check.py
@@ -0,0 +1,45 @@
+from pytest import fixture
+from starlette.testclient import TestClient
+
+from src.application.entity.health_check import Status
+from src.application.entity.service import Service
+from src.application.infrastructure.persistence.in_memory import InMemoryDatabase
+from src.application.infrastructure.web.entity.route import Route
+from src.application.infrastructure.web.rest_api.starlette import StarletteRestApi
+
+
+@fixture(scope="function")
+def setup():
+    db = InMemoryDatabase(
+        config=None
+    )
+    api = StarletteRestApi(
+        config=None,
+        routes=[
+            Route(
+                url="/health",
+                methods=["GET"],
+                handler=StarletteRestApi.health_check,
+                args=None,
+                kwargs=dict(services=[
+                    Service(
+                        service_instance=db
+                    )
+                ])
+            )
+        ]
+    )
+    test_api = TestClient(app=api.__dict__["_StarletteRestApi__app"])
+
+    yield test_api, db
+    del api, test_api
+
+
+def test_valid_health_check(setup):
+    api, db = setup
+    api: TestClient
+    db: InMemoryDatabase
+
+    assert api.get(
+        url="/health"
+    ).json() == {db.__class__.__name__: Status.HEALTHY.name}
