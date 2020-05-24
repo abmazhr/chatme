25b1521d9705ff770fa89dff2aea7333261a569d
Abdulrahman Mazhar <abmazhr@gmail.com>
2020-05-24 20:21:04 +0200

    Update Failure entity to be a dataclass with compatibility with marshmallow serialization

--- users/src/domain/entity/failure.py
+++ users/src/domain/entity/failure.py
@@ -1,5 +1,18 @@
-from src.domain.types import NamedTuple
+import marshmallow_dataclass
+
+from src.domain.types import dataclass, Dict
 
 
-class Failure(NamedTuple):
+@dataclass(frozen=True)
+class Failure:
     error: str
+
+    def as_dict(self) -> Dict[str, str]:
+        return dict(
+            error=self.error
+        )
+
+
+# compatibility with marshmallow serialization
+# maybe making it better later ;)
+marshmallow_dataclass.class_schema(Failure)
