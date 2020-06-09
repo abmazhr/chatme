6ad3a99ab7ed4436643b9ed079fa60ecabafc7a9
Abdulrahman Mazhar <abmazhr@gmail.com>
2020-06-09 03:11:08 +0200

    Updates

--- text_chat/dist/domain/entity/success.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// tslint:disable-next-line:interface-name no-empty-interface
-class Success {
-}
-exports.default = Success;
-//# sourceMappingURL=success.js.map
